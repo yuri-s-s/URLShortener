a240d72265fa7b2f21f5b88f830ab1f99e06b27f
Yuri Souza <yuri.santos@ccc.ufcg.edu.br>
2022-02-09 13:06:43 -0300

    Cors config

--- src/main/java/com/urlShortener/UrlShortenerApplication.java
+++ src/main/java/com/urlShortener/UrlShortenerApplication.java
@@ -4,26 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class UrlShortenerApplication {
-
-    @Bean
-    public FilterRegistrationBean corsFilter1() {
-        UrlBasedCorsConfigurationSource source = new
-                UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new
-                CorsConfiguration().applyPermitDefaultValues();
-        config.addAllowedMethod("*");
-        source.registerCorsConfiguration("/**", config);
-        FilterRegistrationBean bean
-                = new FilterRegistrationBean(new CorsFilter(source));
-        bean.setOrder(0);
-        return bean;
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(UrlShortenerApplication.class, args);
